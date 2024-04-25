8543fc80cbd63ff321ea4d1ec9e23345dbcb1cd6
Daniel <daniel.somji@gmail.com>
2024-04-25 17:26:49 +0200

    Added date_added to adminview of Notizen model

--- budo_database/budo_app/admin.py
+++ budo_database/budo_app/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import Kinder, Turnus, Schwerpunkte, Auslagerorte, Notizen, Document
 
+
+class NotizenAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_added',)
+
 # Register your models here.
+
 
 admin.site.register(Kinder)
 admin.site.register(Turnus)
 admin.site.register(Schwerpunkte)
 admin.site.register(Auslagerorte)
-admin.site.register(Notizen)
+admin.site.register(Notizen, NotizenAdmin)
 admin.site.register(Document)
