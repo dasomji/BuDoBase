e8bbfe48b035d342d8e50eda778f765956c5eacf
Daniel <daniel.somji@gmail.com>
2024-04-25 17:19:37 +0200

    Added widgets to check-in Form

--- budo_database/budo_app/forms.py
+++ budo_database/budo_app/forms.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.forms import ModelForm, Form
 from .models import Kinder, Notizen
 from django import forms
@@ -25,12 +26,25 @@ class NotizForm(forms.ModelForm):
         model = Notizen
         fields = ['notiz']
 
+        widgets = {
+            "notiz": forms.TextInput(attrs={'class': 'w3-input'})
+        }
+
 
 class CheckKindIn(forms.ModelForm):
     class Meta:
+        today = datetime.today().strftime('%Y-%m-%d')
         model = Kinder
         fields = ['check_in_date', 'ausweis', 'e_card', 'einverstaendnis_erklaerung',
                   'taschengeld', 'late_anreise']
+
+        widgets = {
+            'check_in_date': forms.DateInput(attrs={'type': 'date', 'class': 'w3-input', "value": f"{today}"}),
+            'ausweis': forms.CheckboxInput(attrs={'class': 'w3-check w3-margin-left'}),
+            'e_card': forms.CheckboxInput(attrs={'class': 'w3-check w3-margin-left'}),
+            'einverstaendnis_erklaerung': forms.CheckboxInput(attrs={'class': 'w3-check w3-margin-left'}),
+            'taschengeld': forms.TextInput(attrs={'class': 'w3-input'})
+        }
 
 
 class NeuerCheckIn(ModelForm):
