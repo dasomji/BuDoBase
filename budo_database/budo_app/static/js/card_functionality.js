f9033250fa9780f9b88e8ee2fd0a78f6f7d1527d
Daniel <daniel.somji@gmail.com>
2024-05-03 14:41:23 +0200

    [fix] Cards showed + instead of - icon and vicaversa

--- budo_database/budo_app/static/js/card_functionality.js
+++ budo_database/budo_app/static/js/card_functionality.js
@@ -12,20 +12,20 @@ $(document).ready(function () {
     }
     // Show open cards
     $('.card:not(.closed-card) .card-info-container').slideToggle();
-    $('.card:not(.closed-card) .vertical-line').fadeToggle();
+    // $('.card:not(.closed-card) .vertical-line').fadeToggle();
 
     // Save state to cookie when a card is clicked
 
     $('.info-header-container').click(function () {
         let cardInfoContainer = $(this).next('.card-info-container');
-        let icon = $(this).find('.vertical-line');
+        // let icon = $(this).find('.vertical-line');
         let parentContainer = $(this).parent();
 
-        icon.fadeToggle()
+        // icon.fadeToggle()
         cardInfoContainer.slideToggle();
         parentContainer.toggleClass('closed-card');
 
-        var closedCards = $('.closed-card').map(function () {
+        let closedCards = $('.closed-card').map(function () {
             return this.id;
         }).get();
         Cookies.set('closedCards', JSON.stringify(closedCards));
