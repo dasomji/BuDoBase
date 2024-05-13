205aeea8da62c4a696a6b592a4f4222a61045377
Daniel <daniel.somji@gmail.com>
2024-05-13 17:02:00 +0200

    [fix] mobile and tablet-header was wonky

--- budo_database/budo_app/static/js/header.js
+++ budo_database/budo_app/static/js/header.js
@@ -1,10 +1,14 @@
-$(window).on('resize', function () {
-    if ($('#header-content').height() >= 53) {
-        $('#headertitle').css('order', 10);
-    } else {
-        $('#headertitle').css('order', ''); // Reset to default order
-    }
-}).trigger('resize'); // Trigger the function on page load
+// $(window).on('resize', function () {
+//     if ($('#header-content').height() >= 48) {
+//         // $('#headertitle').css('order', 10);
+//         $('#headertitle').hide();
+//         $('#headerbutton').css('order', 11);
+//     } else {
+//         $('#headerbutton').css('order', ''); // Reset to default order
+//         $('#headertitle').show();
+//         // $('#headertitle').css('order', ''); // Reset to default order
+//     }
+// }).trigger('resize'); // Trigger the function on page load
 
 // function openNav() {
 //     document.getElementById("sidebar-container").style.display = "flex";
