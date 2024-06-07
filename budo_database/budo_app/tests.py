ad6a1f79e5ed103a72b60d92f396312c6c617ba4
Daniel <daniel.somji@gmail.com>
2024-06-07 11:55:50 +0200

    added Navbar items

--- budo_database/budo_app/tests.py
+++ budo_database/budo_app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import Turnus
+import os
 
-# Create your tests here.
+
+class TurnusUploadTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('uploadFile')
+        self.test_file_path = os.path.join(
+            os.path.dirname(__file__), 'test_data.xlsx')
+        self.test_file = SimpleUploadedFile(
+            name='test_data.xlsx',
+            content=open(self.test_file_path, 'rb').read(),
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+
+    def test_turnus_upload_and_process(self):
+        response = self.client.post(self.url, {'uploadedFile': self.test_file})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(Turnus.objects.exists())
+        turnus = Turnus.objects.last()
+        self.assertIsNotNone(turnus.uploadedFile)
+        self.assertTrue(os.path.exists(turnus.uploadedFile.path))
